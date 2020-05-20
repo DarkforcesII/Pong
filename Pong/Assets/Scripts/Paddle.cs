6ac782a2269d271a0d6999e4dc7c6c301c17455d
DarkforcesII <darkforcesii@gmail.com>
2020-05-20 13:25:10 -0700

    Working Paddle

---
+++ Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private float height;
+
+    string input;
+    bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+        speed = 5;
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle)
+        {
+            isRight = isRightPaddle;
+
+            // right side
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x;
+
+            input = "PaddleRight";
+        }
+        else
+        {
+            // left side
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+
+            input = "PaddleLeft";
+        }
+        // update paddle position
+        transform.position = pos;
+
+        transform.name = input;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        transform.Translate(move * Vector2.up);
+    }
+}
